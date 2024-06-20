6c391e7397f1cde11f4b0a813af7db02e7e51703
Ashwin Rai <ashwin.2201098cs@iiitbh.ac.in>
2024-06-20 07:47:02 +0530

    Remove unused files and dependencies

--- src/App/Store.js
+++
@@ -1,8 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit';
-import  hamburgerReducer from '../features/hamburger/hamburger';
-export const store = configureStore({
-    reducer: {
-        hamburger:hamburgerReducer,
-        
-    },
-})
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/MyTube-1/',
-  plugins: [react()],
-})
